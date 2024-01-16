98f032e98112958c4e48306e0519a4313070e0f0
Lau <lauraortizgarrido04@gmail.com>
2024-01-16 16:56:55 +0100

    update conflicto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
+ 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FormsComponent } from './forms/forms.component';
 import { EnigmaOfMurdersComponent } from './enigma-of-murders/enigma-of-murders.component';
-
+ 
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,11 +18,13 @@ import { EnigmaOfMurdersComponent } from './enigma-of-murders/enigma-of-murders.
     FooterComponent,
     AboutUsComponent,
     ContactUsComponent,
+    FormsComponent,
     EnigmaOfMurdersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
