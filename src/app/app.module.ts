219c39efb7ec9ea325ef5ac63b1dfb5be8e4b22a
Aakriti313 <dobleagueber313@gmail.com>
2024-01-23 17:01:50 +0100

    revisión version
    correción del footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from'@angular/common/http';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatIconModule } from '@angular/material/icon';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -26,10 +23,7 @@ import { EnigmaOfMurdersComponent } from './enigma-of-murders/enigma-of-murders.
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule,
-    NoopAnimationsModule,
-    MatSlideToggleModule,
-    MatIconModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
